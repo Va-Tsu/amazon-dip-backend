99a04daa5c7d9db1b5e1f7978c66feb851770e01
krasutskaaa <krasutskadiana@gmail.com>
2026-01-31 17:20:57 +0200

    An email service was developed

---
+++ backend/AmazonClone.Application/Services/EmailService.cs
@@ -0,0 +1,24 @@
+using Microsoft.Extensions.Configuration;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+
+namespace AmazonClone.Application.Services;
+
+public class EmailService
+{
+    private readonly IConfiguration _config;
+
+    public EmailService(IConfiguration config) => _config = config;
+    public async Task SendAsync(string email, string subject, string content)
+    {
+        var client = new SendGridClient(_config["SendGrid:ApiKey"]);
+        var from = new EmailAddress(
+            _config["SendGrid:FromEmail"],
+            _config["SendGrid:FromName"]);
+
+        var to = new EmailAddress(email);
+        var msg = MailHelper.CreateSingleEmail(from, to, subject, "", content);
+
+        await client.SendEmailAsync(msg);
+    }
+}
