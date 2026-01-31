687ced09a6fe73c136cb753838e18c76a53af724
krasutskaaa <krasutskadiana@gmail.com>
2026-01-31 17:13:16 +0200

    A revoked token entity was added

---
+++ backend/AmazonClone.Domain/Entities/RevokedToken.cs
@@ -0,0 +1,8 @@
+namespace AmazonClone.Domain.Entities;
+
+public class RevokedToken
+{
+    public Guid Id { get; set; }
+    public string Token { get; set; } = null!;
+    public DateTime Expiration { get; set; }
+}
