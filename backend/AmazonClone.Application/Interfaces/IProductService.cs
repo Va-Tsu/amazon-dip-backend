7a1d11b254e40bf82fd6a25077347b657e2796ac
krasutskaaa <krasutskadiana@gmail.com>
2026-01-31 22:13:14 +0200

    An interface for a future product service was created

---
+++ backend/AmazonClone.Application/Interfaces/IProductService.cs
@@ -0,0 +1,14 @@
+using AmazonClone.Domain.Entities;
+
+namespace AmazonClone.Application.Interfaces;
+
+public interface IProductService
+{
+    Task<List<Product>> GetAllAsync();
+    Task<Product?> GetByIdAsync(Guid id);
+    Task<List<Product>> GetByCategoryIdAsync(int categoryId);
+    Task<List<Product>> SearchAsync(string query);
+    Task<List<Product>> GetNewAsync();
+    Task<List<Product>> GetRecommendedAsync();
+
+}
