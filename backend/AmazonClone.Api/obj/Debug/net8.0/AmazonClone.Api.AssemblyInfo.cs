7d8035816bd6e453964f73db943df475753a8efc
krasutskaaa <krasutskadiana@gmail.com>
2026-01-31 19:16:47 +0200

    Missing packages were added

--- backend/AmazonClone.Api/obj/Debug/net8.0/AmazonClone.Api.AssemblyInfo.cs
+++ backend/AmazonClone.Api/obj/Debug/net8.0/AmazonClone.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AmazonClone.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32cd61d0ea2cfb41fc3499ea9afe95b1dd8fd65b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7538463c953950fce55db27f1d9f85b9101e747")]
 [assembly: System.Reflection.AssemblyProductAttribute("AmazonClone.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AmazonClone.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- backend/AmazonClone.Infrastructure/obj/Debug/net8.0/AmazonClone.Infrastructure.AssemblyInfo.cs
+++ backend/AmazonClone.Infrastructure/obj/Debug/net8.0/AmazonClone.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AmazonClone.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47a0bc1e9e45b974ce9bbfd80518fbb03a5f51f7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7538463c953950fce55db27f1d9f85b9101e747")]
 [assembly: System.Reflection.AssemblyProductAttribute("AmazonClone.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AmazonClone.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
