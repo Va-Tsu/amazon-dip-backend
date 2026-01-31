cdc2318f2135f5f79ad881d73e992f372213f422
krasutskaaa <krasutskadiana@gmail.com>
2026-01-31 21:25:57 +0200

    A product entity was realised

---
+++ backend/AmazonClone.Domain/Entities/Product.cs
@@ -0,0 +1,19 @@
+namespace AmazonClone.Domain.Entities;
+
+public class Product
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public decimal Weight { get; set; }
+    public decimal Price { get; set; }
+    public string ImageUrl { get; set; }
+    public int CategoryId { get; set; }
+    public Category Category { get; set; }
+    public int CountryId { get; set; }
+    public Country Country { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public bool IsActive { get; set; } = true;
+
+
+}
