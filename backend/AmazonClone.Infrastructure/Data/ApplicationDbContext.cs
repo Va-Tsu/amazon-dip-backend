7934cade1c5e8ccc991747c37cb67d66a57186b6
krasutskaaa <krasutskadiana@gmail.com>
2026-01-31 17:59:48 +0200

    The appication db context was implemented

---
+++ backend/AmazonClone.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+using AmazonClone.Domain.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AmazonClone.Infrastructure.Data;
+
+public class ApplicationDbContext: IdentityDbContext<IdentityUser>
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+    
+    public DbSet<User> Users { get; set; }
+    public DbSet<RevokedToken> RevokedTokens { get; set; }
+    
+}
