0039866d95fff58a478611b056071e9ba61213c8
krasutskaaa <krasutskadiana@gmail.com>
2026-01-31 21:27:30 +0200

    A category entity was created

---
+++ backend/AmazonClone.Domain/Entities/Category.cs
@@ -0,0 +1,9 @@
+namespace AmazonClone.Domain.Entities;
+
+public class Category
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string ImageUrl { get; set; }
+    public ICollection<Product>Products { get; set; }
+}
