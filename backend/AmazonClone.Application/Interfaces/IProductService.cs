c96601c77565596e6bd2dc6c15fad08ebb757ab6
krasutskaaa <krasutskadiana@gmail.com>
2026-02-03 22:39:28 +0200

    Missed method of searching products by a country id was implemented

--- backend/AmazonClone.Application/Interfaces/IProductService.cs
+++ backend/AmazonClone.Application/Interfaces/IProductService.cs
@@ -7,6 +7,7 @@ public interface IProductService
     Task<List<Product>> GetAllAsync();
     Task<Product?> GetByIdAsync(Guid id);
     Task<List<Product>> GetByCategoryIdAsync(int categoryId);
+    Task<List<Product>> GetByCountryIdAsync(int countryId);
     Task<List<Product>> SearchAsync(string query);
     Task<List<Product>> GetNewAsync();
     Task<List<Product>> GetRecommendedAsync();
