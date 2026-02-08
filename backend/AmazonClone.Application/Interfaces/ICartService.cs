d62181d6ca797038cfdad9904071d248e7e5b46e
krasutskaaa <krasutskadiana@gmail.com>
2026-02-09 00:00:58 +0200

    An interface for a future cart service was implemented

---
+++ backend/AmazonClone.Application/Interfaces/ICartService.cs
@@ -0,0 +1,10 @@
+using AmazonClone.Domain.Entities;
+
+namespace AmazonClone.Application.Interfaces;
+
+public interface ICartService
+{
+    Task<Cart> GetOrCreateCartAsync(string identityUserId);
+    Task AddProductAync(string identityUserId, Guid productId, int quantity);
+    
+}
