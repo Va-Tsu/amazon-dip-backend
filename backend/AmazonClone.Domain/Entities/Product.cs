59129fb53362e9e6f18c76e65e8082ba039be2f3
krasutskaaa <krasutskadiana@gmail.com>
2026-02-08 21:11:58 +0200

    The fields Category and Country can't be null, so it was declared

--- backend/AmazonClone.Domain/Entities/Product.cs
+++ backend/AmazonClone.Domain/Entities/Product.cs
@@ -9,9 +9,9 @@ public class Product
     public decimal Price { get; set; }
     public string ImageUrl { get; set; }
     public int CategoryId { get; set; }
-    public Category Category { get; set; }
+    public Category Category { get; set; } = null!;
     public int CountryId { get; set; }
-    public Country Country { get; set; }
+    public Country Country { get; set; } = null!;
     public DateTime CreatedAt { get; set; }
     public bool IsActive { get; set; } = true;
 
