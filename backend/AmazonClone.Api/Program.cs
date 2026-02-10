92aa7c12b4e24266d632697bfb80d5c082279bfc
krasutskaaa <krasutskadiana@gmail.com>
2026-02-10 22:05:01 +0200

    Email uniqueness was set as a mandatory condition in the identity service registration in dependency injection

--- backend/AmazonClone.Api/Program.cs
+++ backend/AmazonClone.Api/Program.cs
@@ -54,6 +54,7 @@ builder.Services.AddControllers()
 builder.Services.AddScoped<IEmailService, EmailService>();
 builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
     {
+        options.User.RequireUniqueEmail = true;
         options.SignIn.RequireConfirmedEmail = true;
     })
     .AddEntityFrameworkStores<ApplicationDbContext>()
