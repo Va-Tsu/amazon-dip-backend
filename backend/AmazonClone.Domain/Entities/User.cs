b756388f63b956175313f9da77c62e6b20cc9248
krasutskaaa <krasutskadiana@gmail.com>
2026-02-10 21:50:10 +0200

    Fields were changed according to the marketplace registration process

--- backend/AmazonClone.Domain/Entities/User.cs
+++ backend/AmazonClone.Domain/Entities/User.cs
@@ -4,9 +4,6 @@ public class User
 {
     public Guid Id { get; set; }
     public string? IdentityUserId { get; set; }
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public string Username { get; set; }
+    public string FullName { get; set; }
     public string Email { get; set; }
-    public DateOnly Birthday { get; set; }
 }
