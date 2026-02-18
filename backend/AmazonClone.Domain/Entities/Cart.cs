134c0b262a2a2b3f4c1d031da249933812ab8ee0
krasutskaaa <krasutskadiana@gmail.com>
2026-02-18 22:41:53 +0200

    The cart entity fields were changed

--- backend/AmazonClone.Domain/Entities/Cart.cs
+++ backend/AmazonClone.Domain/Entities/Cart.cs
@@ -7,7 +7,7 @@ public class Cart
     public Guid Id { get; set;}
     public string? IdentityUserId { get;set ;}
     public string? SessionId { get;set;}
+    public decimal TotalPrice { get;set;}
     public List<CartItem> Items { get; set; } = new();
-
-    [NotMapped] public decimal TotalSum => Items.Sum(i => i.TotalPrice);
+    
 }
