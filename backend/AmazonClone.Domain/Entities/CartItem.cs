3687cd431a0f3641ff37342fee98e43e78c4e363
krasutskaaa <krasutskadiana@gmail.com>
2026-02-18 22:43:34 +0200

    The cart item entity fields were modified

--- backend/AmazonClone.Domain/Entities/CartItem.cs
+++ backend/AmazonClone.Domain/Entities/CartItem.cs
@@ -6,10 +6,11 @@ public class CartItem
 {
     public Guid Id { get; set; }
     public Guid CartId { get; set; }
+    public Cart Cart { get; set; }
     public Guid ProductId { get; set; }
+    public string ProductName { get; set; }
+    public decimal Price { get; set; }
     public int Quantity { get; set; }
-    public Cart Cart { get; set; } = null!;
-    public Product Product { get; set; } = null!;
-    [NotMapped]
-    public decimal TotalPrice => Quantity * Product.Price;
+ 
+    
 }
