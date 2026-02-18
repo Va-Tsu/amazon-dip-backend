2d67e81ae46d27c79e7b2b4a1de88d5beb2339de
krasutskaaa <krasutskadiana@gmail.com>
2026-02-18 22:46:18 +0200

    The new entity for an order item representation was created

---
+++ backend/AmazonClone.Domain/Entities/OrderItem.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AmazonClone.Domain.Entities;
+
+public class OrderItem
+{
+    public Guid Id { get; set; }
+    public Guid OrderId { get; set; }
+    public Guid ProductId { get; set; }
+    public string ProductName { get; set; }
+    public decimal Price { get; set; }
+    public int Quantity { get; set; }
+    
+    
+}
