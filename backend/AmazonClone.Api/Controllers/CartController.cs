65a6988546b40e795f52141a3c589b08160c2eb0
krasutskaaa <krasutskadiana@gmail.com>
2026-02-18 22:58:59 +0200

    New endpoinds were added based on the new methods in the cart service

--- backend/AmazonClone.Api/Controllers/CartController.cs
+++ backend/AmazonClone.Api/Controllers/CartController.cs
@@ -1,8 +1,10 @@
+using System.Security.Claims;
 using AmazonClone.Application.Interfaces;
 using AmazonClone.Domain.Entities;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Rewrite;
 
 namespace AmazonClone.Api.Controllers;
 
@@ -23,19 +25,43 @@ public class CartController : ControllerBase
 
     [Authorize]
     [HttpGet]
-    public async Task<ActionResult> GetCart()
+    public async Task<ActionResult> Get()
     {
-        var identityUserId = (await _userManager.GetUserAsync(User))?.Id;
-        var cart = await _cartService.GetOrCreateCartAsync(identityUserId);
-        return Ok(cart);
+        var identityUserId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+        return Ok(await _cartService.GetMyCartAsync(identityUserId));
     }
 
     [Authorize]
     [HttpPost("add")]
-    public async Task<IActionResult> AddToCart(Guid productId, int quantity = 1)
+    public async Task<IActionResult> AddItem(CartItem cartItem)
     {
-        var identityUserId = (await _userManager.GetUserAsync(User))?.Id;
-        await _cartService.AddProductAync(identityUserId, productId, quantity);
+        var identityUserId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+        await _cartService.AddItemAsync(identityUserId, cartItem.ProductId, cartItem.Quantity);
+        return Ok();
+    }
+
+    [HttpPut("update/{id}")]
+    public async Task<IActionResult> Update(Guid itemId, int quantity)
+    {
+        var identityUserId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+        await _cartService.UpdateItemAsync(identityUserId, itemId, quantity);
+        return Ok();
+        
+    }
+
+    [HttpDelete("remove/{id}")]
+    public async Task<IActionResult> Remove(Guid itemId)
+    {
+        var identityUserId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+        await _cartService.RemoveItemAsync(identityUserId, itemId);
+        return Ok();
+    }
+
+    [HttpDelete("clear")]
+    public async Task<IActionResult> Clear()
+    {
+        var identityUserId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+        await _cartService.ClearAsync(identityUserId);
         return Ok();
     }
 }
