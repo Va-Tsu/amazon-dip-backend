e188b513ffaa587c087f610fc76ddeef7a6cc97a
krasutskaaa <krasutskadiana@gmail.com>
2026-02-22 21:22:40 +0200

    The names of IdentityUserId fields were changed to UserId

--- backend/AmazonClone.Application/Interfaces/ICartService.cs
+++ backend/AmazonClone.Application/Interfaces/ICartService.cs
@@ -4,10 +4,10 @@ namespace AmazonClone.Application.Interfaces;
 
 public interface ICartService
 {
-    Task<Cart?> GetMyCartAsync(string identityUserId);
-    Task AddItemAsync(string identityUserId, Guid productId, int quantity);
-    Task UpdateItemAsync(string identityUserId, Guid cartItemId, int quantity);
-    Task RemoveItemAsync(string identityUserId, Guid cartItemId);
-    Task ClearAsync(string identityUserId);
+    Task<Cart?> GetMyCartAsync(string UserId);
+    Task AddItemAsync(string UserId, Guid productId, int quantity);
+    Task UpdateItemAsync(string UserId, Guid cartItemId, int quantity);
+    Task RemoveItemAsync(string UserId, Guid cartItemId);
+    Task ClearAsync(string UserId);
     
 }
--- backend/AmazonClone.Application/Interfaces/IOrderService.cs
+++ backend/AmazonClone.Application/Interfaces/IOrderService.cs
@@ -4,8 +4,8 @@ namespace AmazonClone.Application.Interfaces;
 
 public interface IOrderService
 {
-    Task<Order> CreateAsync(string identityUserId, List<OrderItem> orderItems);
-    Task<List<Order>> GetMyOrdersAsync(string identityUserId);
-    Task<Order?> GetByIdAsync(Guid orderId, string identityUserId);
+    Task<Order> CreateAsync(string UserId, List<OrderItem> orderItems);
+    Task<List<Order>> GetMyOrdersAsync(string UserId);
+    Task<Order?> GetByIdAsync(Guid orderId, string UserId);
     
 }
