3a1b92eef07f69a20ec0e31b08529ca3d5aa60bd
krasutskaaa <krasutskadiana@gmail.com>
2026-02-22 21:15:07 +0200

    A field name was changed according to modifications in the user entity

--- backend/AmazonClone.Domain/Entities/Cart.cs
+++ backend/AmazonClone.Domain/Entities/Cart.cs
@@ -5,7 +5,7 @@ namespace AmazonClone.Domain.Entities;
 public class Cart
 {
     public Guid Id { get; set;}
-    public string? IdentityUserId { get;set ;}
+    public string? UserId { get;set ;}
     public string? SessionId { get;set;}
     public decimal TotalPrice { get;set;}
     public List<CartItem> Items { get; set; } = new();
