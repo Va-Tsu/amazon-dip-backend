459751f411b074dc616ff715136b2b4c0c2ace89
krasutskaaa <krasutskadiana@gmail.com>
2026-02-22 22:33:00 +0200

    The cart interface was developed to be login independent

--- backend/AmazonClone.Application/Interfaces/ICartService.cs
+++ backend/AmazonClone.Application/Interfaces/ICartService.cs
@@ -4,10 +4,11 @@ namespace AmazonClone.Application.Interfaces;
 
 public interface ICartService
 {
-    Task<Cart?> GetMyCartAsync(string UserId);
-    Task AddItemAsync(string UserId, Guid productId, int quantity);
-    Task UpdateItemAsync(string UserId, Guid cartItemId, int quantity);
-    Task RemoveItemAsync(string UserId, Guid cartItemId);
-    Task ClearAsync(string UserId);
-    
+    Task<Cart?> GetMyCartAsync(string userId, string sessionId, CancellationToken ct = default);
+    Task AddItemAsync(string userId, string sessionId, Guid productId, int quantity, CancellationToken ct = default);
+    Task UpdateItemAsync(string userId, string sessionId, Guid cartItemId, int quantity, CancellationToken ct = default);
+    Task RemoveItemAsync(string userId, string sessionId, Guid cartItemId, CancellationToken ct = default);
+    Task ClearAsync(string userId, string sessionId, CancellationToken ct = default);
+    Task MergeGuestCartIntoUserAsync(string userId, string cartKey, CancellationToken ct = default);
 }
+    
