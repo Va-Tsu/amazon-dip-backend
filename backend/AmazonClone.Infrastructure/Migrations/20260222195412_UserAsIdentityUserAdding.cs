5ea93cdb5d0562dc3c7213587d4f06d008d8688f
krasutskaaa <krasutskadiana@gmail.com>
2026-02-22 21:59:42 +0200

    A migration was created as the user, cart and order entities were modified

---
+++ backend/AmazonClone.Infrastructure/Migrations/20260222195412_UserAsIdentityUserAdding.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AmazonClone.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserAsIdentityUserAdding : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.RenameColumn(
+                name: "IdentityUserId",
+                table: "Orders",
+                newName: "UserId");
+
+            migrationBuilder.RenameColumn(
+                name: "IdentityUserId",
+                table: "Carts",
+                newName: "UserId");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FullName",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FullName",
+                table: "AspNetUsers");
+
+            migrationBuilder.RenameColumn(
+                name: "UserId",
+                table: "Orders",
+                newName: "IdentityUserId");
+
+            migrationBuilder.RenameColumn(
+                name: "UserId",
+                table: "Carts",
+                newName: "IdentityUserId");
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FullName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IdentityUserId = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+        }
+    }
+}
