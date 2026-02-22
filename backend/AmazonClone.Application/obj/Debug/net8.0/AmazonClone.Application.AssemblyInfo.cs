4f2b5a109943cd606fc81f1a8d4b43c5c82b7059
krasutskaaa <krasutskadiana@gmail.com>
2026-02-22 21:18:03 +0200

    A reference to a nuget library was added to the AmazonClone.Domain

--- backend/AmazonClone.Application/obj/Debug/net8.0/AmazonClone.Application.AssemblyInfo.cs
+++ backend/AmazonClone.Application/obj/Debug/net8.0/AmazonClone.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AmazonClone.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+580aaffec35953e360469b199a887f6311ca77c6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ffe1048d9c69f2f9495e5e241abcacc6e531ee1")]
 [assembly: System.Reflection.AssemblyProductAttribute("AmazonClone.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AmazonClone.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
