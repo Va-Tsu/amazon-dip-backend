e6ca31692fc31b573d363096e6cc4f9cd27ca307
krasutskaaa <krasutskadiana@gmail.com>
2026-02-27 15:09:51 +0200

    The field SessionId was renamed to CartKey

--- backend/AmazonClone.Domain/Entities/Cart.cs
+++ backend/AmazonClone.Domain/Entities/Cart.cs
@@ -6,7 +6,7 @@ public class Cart
 {
     public Guid Id { get; set;}
     public string? UserId { get;set ;}
-    public string? SessionId { get;set;}
+    public string? CartKey { get;set;}
     public decimal TotalPrice { get;set;}
     public List<CartItem> Items { get; set; } = new();
     
