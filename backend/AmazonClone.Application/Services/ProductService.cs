618cac758e2897b635a160da1a1c41c5a78f8bc7
krasutskaaa <krasutskadiana@gmail.com>
2026-03-14 10:00:43 +0200

    The methods for the recently viewed product logic were realized in the product service

--- backend/AmazonClone.Application/Services/ProductService.cs
+++ backend/AmazonClone.Application/Services/ProductService.cs
@@ -68,9 +68,126 @@ public class ProductService : IProductService
             .ToListAsync();
     }
 
-    public Task<List<Product>> GetRecommendedAsync()
+    public async Task<(List<Product>, bool hasMore)> GetRecommendedAsync(string? userId,
+        int page, int pageSize, CancellationToken ct = default)
     {
-        throw new NotImplementedException();
+        page = page < 1 ? 1 : page;
+        pageSize = pageSize <= 0 ? 8 : pageSize;
+        
+        var skip = (page - 1) * pageSize;
+        var query = _dbContext.Products
+            .AsNoTracking()
+            .Where(p => p.IsActive);
+        if (string.IsNullOrEmpty(userId))
+        {
+            query = query.OrderByDescending(p => p.CreatedAt);
+            var products = await query.Skip(skip)
+                .Take(pageSize + 1)
+                .ToListAsync(ct);
+            
+            var hasMore = products.Count>pageSize;
+
+            if (hasMore)
+            {
+                products = products.Take(pageSize).ToList();
+            }
+            
+            return (products, hasMore);
+        }
+
+        var oldViewed = await _dbContext.RecentlyViewedProducts
+            .Where(p => p.UserId == userId)
+            .OrderByDescending(r => r.ViewedAt)
+            .Skip(50)
+            .ToListAsync(ct);
+        
+        if (oldViewed.Any())
+        {
+            _dbContext.RecentlyViewedProducts.RemoveRange(oldViewed);
+            await _dbContext.SaveChangesAsync(ct);
+        }
+        
+        var recentViewedIds = await _dbContext.RecentlyViewedProducts
+            .AsNoTracking()
+            .Where(p => p.UserId == userId)
+            .OrderByDescending(r => r.ViewedAt)
+            .Take(10)
+            .Select(r => r.ProductId)
+            .ToListAsync(ct);
+
+        if (!recentViewedIds.Any())
+        {
+            query = query.OrderByDescending(p => p.CreatedAt);
+            var products = await query.Skip(skip)
+                .Take(pageSize + 1)
+                .ToListAsync(ct);
+            
+            var hasMore = products.Count>pageSize;
+
+            if (hasMore)
+            {
+                products = products.Take(pageSize).ToList();
+            }
+            return (products, hasMore);
+        }
+
+        var viewedMeta = await _dbContext.Products
+            .AsNoTracking().Where(p => recentViewedIds.Contains(p.Id))
+            .Select(p => new
+            {
+                p.CategoryId,
+                p.CountryId
+            })
+            .ToListAsync(ct);
+        
+        var categoryIds = viewedMeta
+            .Select(p => p.CategoryId)
+            .Distinct()
+            .ToList();
+        var countryIds = viewedMeta
+            .Select(p => p.CountryId)
+            .Distinct()
+            .ToList();
+        
+        query = query.Where(p => !recentViewedIds.Contains(p.Id))
+            .OrderByDescending(p=>
+                (categoryIds.Contains(p.CategoryId)? 2 : 0)+
+                (countryIds.Contains(p.CountryId) ? 1 : 0))
+            .ThenByDescending(p=>p.CreatedAt);
+        var result =  await query
+            .Skip(skip)
+            .Take(pageSize+1)
+            .ToListAsync(ct);
+        var hasMoreResult = result.Count>pageSize;
+        if (hasMoreResult)
+        {
+            result = result.Take(pageSize).ToList();
+        }
+        
+        return(result, hasMoreResult);
+    }
+
+    public async Task AddRecentlyViewedAsync(string userId, Guid productId, CancellationToken ct = default)
+    {
+        var existing = await _dbContext.RecentlyViewedProducts
+            .FirstOrDefaultAsync(r=>r.UserId == userId && 
+                                    r.ProductId == productId, ct);
+        if (existing != null)
+        {
+            existing.ViewedAt = DateTime.UtcNow;
+        }
+        else
+        {
+            _dbContext.RecentlyViewedProducts.Add(
+                new RecentlyViewedProduct
+                {
+                    Id = Guid.NewGuid(),
+                    UserId = userId,
+                    ProductId = productId,
+                    ViewedAt = DateTime.UtcNow
+                });
+        }
+        await _dbContext.SaveChangesAsync(ct);
     }
 
     public async Task<Guid> CreateAsync(Product product, CancellationToken ct = default)
