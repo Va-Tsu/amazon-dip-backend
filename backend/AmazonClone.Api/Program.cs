ac4596dce85887eb81b3c820f43eefce1337ade4
krasutskaaa <krasutskadiana@gmail.com>
2026-03-14 16:50:39 +0200

    The discount service was registered in dependecy injection

--- backend/AmazonClone.Api/Program.cs
+++ backend/AmazonClone.Api/Program.cs
@@ -90,6 +90,7 @@ builder.Services.AddScoped<ICartService, CartService>();
 builder.Services.AddScoped<ICategoryService, CategoryService>();
 builder.Services.AddScoped<IOrderService, OrderService>();
 builder.Services.AddScoped<ICountryService, CountryService>();
+builder.Services.AddScoped<IDiscountService, DiscountService>();
 
 
 
