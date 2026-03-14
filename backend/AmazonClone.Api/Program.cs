02185750cea075fa90ee38bfd42b044c036dc3dd
krasutskaaa <krasutskadiana@gmail.com>
2026-03-14 18:19:01 +0200

    The product comment service was registered in dependency injection

--- backend/AmazonClone.Api/Program.cs
+++ backend/AmazonClone.Api/Program.cs
@@ -91,6 +91,7 @@ builder.Services.AddScoped<ICategoryService, CategoryService>();
 builder.Services.AddScoped<IOrderService, OrderService>();
 builder.Services.AddScoped<ICountryService, CountryService>();
 builder.Services.AddScoped<IDiscountService, DiscountService>();
+builder.Services.AddScoped<IProductCommentService, ProductCommentService>();
 
 
 
