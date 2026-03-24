9c1bbbf351452096262d2ce61a61d25f8b320cb0
krasutskaaa <krasutskadiana@gmail.com>
2026-03-24 23:25:23 +0200

    A product filed was added to the order item entity

--- backend/AmazonClone.Domain/Entities/OrderItem.cs
+++ backend/AmazonClone.Domain/Entities/OrderItem.cs
@@ -6,7 +6,9 @@ public class OrderItem
 {
     public Guid Id { get; set; }
     public Guid OrderId { get; set; }
+    public Order Order { get; set; }
     public Guid ProductId { get; set; }
+    public Product Product { get; set; }
     public string ProductName { get; set; }
     public decimal Price { get; set; }
     public int Quantity { get; set; }
