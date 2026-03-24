009ea5b464b1667d537965064e8d396ab8260314
krasutskaaa <krasutskadiana@gmail.com>
2026-03-24 22:59:01 +0200

    A new seller entity was added

---
+++ backend/AmazonClone.Domain/Entities/Seller.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace AmazonClone.Domain.Entities;
+
+public class Seller
+{
+    public Guid Id { get; set; }
+    public string FullName { get; set; }
+    public string StoreName { get; set; }
+    public string? Description { get; set; }
+    public string? ImageUrl { get; set; }
+    public int CountryId { get; set; }
+    public Country Country { get; set; }
+
+    public string UserId { get; set; } = default!;
+    public User User { get; set; } = default!;
+    public decimal Balance { get; set; }
+    public decimal PandingBalance { get; set; }
+    public bool IsActive { get; set; } =  true;
+    
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    
+    public ICollection<Product> Products { get; set; } = new List<Product>();
+
+}
