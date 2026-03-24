49bc86565de712c8e205caf5b6bf2268d0db8fb3
krasutskaaa <krasutskadiana@gmail.com>
2026-03-24 23:41:06 +0200

    A new SellerProductImageProductParameterAddingAndUserProductCartItemOrderItemUpdating migration was added

---
+++ backend/AmazonClone.Infrastructure/Migrations/20260324213733_SellerProductImageProductParameterAddingAndUserProductCartItemOrderItemUpdating.cs
@@ -0,0 +1,367 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AmazonClone.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class SellerProductImageProductParameterAddingAndUserProductCartItemOrderItemUpdating : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ImageUrl",
+                table: "Products",
+                newName: "SKU");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Article",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Brand",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "CurrentPrice",
+                table: "Products",
+                type: "decimal(18,2)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ExpirationDate",
+                table: "Products",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "HasDiscount",
+                table: "Products",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Ingridients",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsPublished",
+                table: "Products",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<int>(
+                name: "LowStockTreshold",
+                table: "Products",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "OldPrice",
+                table: "Products",
+                type: "decimal(18,2)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "ParcelWeight",
+                table: "Products",
+                type: "decimal(18,2)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "SellerId",
+                table: "Products",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "Products",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "StockQuantity",
+                table: "Products",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "StorageConditions",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "TrackInventory",
+                table: "Products",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "ProductImages",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ProductId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Url = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false),
+                    SortOrder = table.Column<int>(type: "int", nullable: false),
+                    IsMain = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductImages", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ProductImages_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProductParameters",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ProductId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    Value = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductParameters", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ProductParameters_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Sellers",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    FullName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    StoreName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CountryId = table.Column<int>(type: "int", nullable: false),
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Balance = table.Column<decimal>(type: "decimal(18,2)", precision: 18, scale: 2, nullable: false),
+                    PandingBalance = table.Column<decimal>(type: "decimal(18,2)", precision: 18, scale: 2, nullable: false),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sellers", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Sellers_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Sellers_Countries_CountryId",
+                        column: x => x.CountryId,
+                        principalTable: "Countries",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_SellerId",
+                table: "Products",
+                column: "SellerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderItems_ProductId",
+                table: "OrderItems",
+                column: "ProductId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CartItems_ProductId",
+                table: "CartItems",
+                column: "ProductId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductImages_ProductId",
+                table: "ProductImages",
+                column: "ProductId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductImages_ProductId_SortOrder",
+                table: "ProductImages",
+                columns: new[] { "ProductId", "SortOrder" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductParameters_ProductId",
+                table: "ProductParameters",
+                column: "ProductId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sellers_CountryId",
+                table: "Sellers",
+                column: "CountryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sellers_UserId",
+                table: "Sellers",
+                column: "UserId",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CartItems_Products_ProductId",
+                table: "CartItems",
+                column: "ProductId",
+                principalTable: "Products",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderItems_Products_ProductId",
+                table: "OrderItems",
+                column: "ProductId",
+                principalTable: "Products",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Sellers_SellerId",
+                table: "Products",
+                column: "SellerId",
+                principalTable: "Sellers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CartItems_Products_ProductId",
+                table: "CartItems");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderItems_Products_ProductId",
+                table: "OrderItems");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Sellers_SellerId",
+                table: "Products");
+
+            migrationBuilder.DropTable(
+                name: "ProductImages");
+
+            migrationBuilder.DropTable(
+                name: "ProductParameters");
+
+            migrationBuilder.DropTable(
+                name: "Sellers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Products_SellerId",
+                table: "Products");
+
+            migrationBuilder.DropIndex(
+                name: "IX_OrderItems_ProductId",
+                table: "OrderItems");
+
+            migrationBuilder.DropIndex(
+                name: "IX_CartItems_ProductId",
+                table: "CartItems");
+
+            migrationBuilder.DropColumn(
+                name: "Article",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "Brand",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "CurrentPrice",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "ExpirationDate",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "HasDiscount",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "Ingridients",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "IsPublished",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "LowStockTreshold",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "OldPrice",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "ParcelWeight",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "SellerId",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "StockQuantity",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "StorageConditions",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "TrackInventory",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "AspNetUsers");
+
+            migrationBuilder.RenameColumn(
+                name: "SKU",
+                table: "Products",
+                newName: "ImageUrl");
+        }
+    }
+}
