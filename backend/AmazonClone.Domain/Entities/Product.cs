ae6595c20834d852aa821d5589d6f750bacdf16f
krasutskaaa <krasutskadiana@gmail.com>
2026-03-24 23:17:08 +0200

    New fields were added to the product entity

--- backend/AmazonClone.Domain/Entities/Product.cs
+++ backend/AmazonClone.Domain/Entities/Product.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations.Schema;
+using AmazonClone.Domain.Enums;
 
 namespace AmazonClone.Domain.Entities;
 
@@ -7,27 +8,40 @@ public class Product
     public Guid Id { get; set; }
     public string Name { get; set; }
     public string Description { get; set; }
+    public string Brand { get; set; }
     public decimal Weight { get; set; }
+    public decimal? ParcelWeight { get; set; }
+    public string? Article { get; set; }
     public decimal Price { get; set; }
-    public string ImageUrl { get; set; }
     public int CategoryId { get; set; }
     public Category Category { get; set; } = null!;
     public int CountryId { get; set; }
     public Country Country { get; set; } = null!;
+    public bool TrackInventory { get; set; }
+    public int StockQuantity { get; set; }
+    public int? LowStockTreshold { get; set; }
     public DateTime CreatedAt { get; set; }
     public bool IsActive { get; set; } = true;
+    public bool IsPublished { get; set; } = true;
+    public string? StorageConditions { get; set; }
+    public DateTime? ExpirationDate { get; set; }
+    public string? Ingridients { get; set; }
+    public string SKU { get; set; } = "";
+    public decimal? CurrentPrice { get; set; }
+    public decimal? OldPrice { get; set; }
+    public bool HasDiscount { get; set; }
+    public Guid? SellerId { get; set; } = default!;
+    public Seller? Seller { get; set; } = default!;
+    public ProductStatus Status { get; set; }
     
+    public ICollection<ProductImage> Images { get; set; } = new List<ProductImage>();
     public ICollection<Discount> Discounts { get; set; } = new List<Discount>();
     public ICollection<ProductComment> Comments { get; set; } = new List<ProductComment>();
+    public ICollection<ProductParameter> Parameters { get; set; } = new List<ProductParameter>();
+    public ICollection<OrderItem>OrderItems { get; set; } = new List<OrderItem>();
+    public ICollection<CartItem>CartItems { get; set; } = new List<CartItem>();
+
     
-    [NotMapped]
-    public decimal CurrentPrice { get; set; }
-    
-    [NotMapped]
-    public decimal? OldPrice { get; set; }
-    
-    [NotMapped]
-    public bool HasDiscount { get; set; }
 
 
 }
