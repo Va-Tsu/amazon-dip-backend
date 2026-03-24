515abd44a5d277fa2ce32ef79f7aab1eea530f49
krasutskaaa <krasutskadiana@gmail.com>
2026-03-24 23:07:36 +0200

    A field seller was created in the user entity

--- backend/AmazonClone.Domain/Entities/User.cs
+++ backend/AmazonClone.Domain/Entities/User.cs
@@ -5,6 +5,9 @@ namespace AmazonClone.Domain.Entities;
 public class User : IdentityUser
 {
     public string FullName { get; set; }
+    public string? ImageUrl { get; set; }
+    
+    public Seller? Seller { get; set; }
     
     public ICollection<RecentlyViewedProduct> RecentlyViewedProducts { get; set; } = new List<RecentlyViewedProduct>();
     public ICollection<ProductComment> ProductComments { get; set; } = new List<ProductComment>();
