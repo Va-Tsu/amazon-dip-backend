7fe669ce9ba3fb7916295d85cc7cdf47e65482a8
krasutskaaa <krasutskadiana@gmail.com>
2026-04-04 22:27:04 +0300

    Methods templates for admin create, update and delete were developed; the GetProductByCategoryIdAsync method template was updated with new filter fields

--- backend/AmazonClone.Application/Interfaces/ICategoryService.cs
+++ backend/AmazonClone.Application/Interfaces/ICategoryService.cs
@@ -4,8 +4,12 @@ namespace AmazonClone.Application.Interfaces;
 
 public interface ICategoryService
 {
-    Task<List<Category>> GetAllAsync();
-    Task<List<Product>> GetProductByCategoryIdAsync(int categoryId, int page = 1, int pageSize = 10,
-        decimal? minPrice = null, decimal? maxPrice = null);
+    Task<int> CreateAsync(string name, string imageUrl, CancellationToken ct);
+    Task UpdateAsync(int id, string? name, string? imageUrl, CancellationToken ct);
+    Task DeleteAsync(int id, CancellationToken ct);
+    Task<List<Category>> GetAllAsync(CancellationToken ct);
+    Task<List<Product>> GetProductByCategoryIdAsync(int categoryId, string? brand ,
+        bool? isAvailable, CancellationToken ct, decimal? minPrice = null,
+        decimal? maxPrice = null, int page = 1, int pageSize = 10);
     
 }
