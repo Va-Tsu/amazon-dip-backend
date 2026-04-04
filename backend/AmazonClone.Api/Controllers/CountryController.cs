d64e76862a779058c31c5f41105fd41b62190cdd
krasutskaaa <krasutskadiana@gmail.com>
2026-04-04 22:39:39 +0300

    New admin endpoints were realized implementing the new service methods; the GetProducts endpoint was updated

--- backend/AmazonClone.Api/Controllers/CountryController.cs
+++ backend/AmazonClone.Api/Controllers/CountryController.cs
@@ -1,4 +1,6 @@
 using AmazonClone.Application.Interfaces;
+using AmazonClone.Domain.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AmazonClone.Api.Controllers;
@@ -17,15 +19,56 @@ public class CountryController: ControllerBase
     }
     
     [HttpGet]
-    public async Task<IActionResult> GetAll()
+    public async Task<IActionResult> GetAll(CancellationToken ct)
     {
-        return Ok(await _countryService.GetAllAsync());
+        return Ok(await _countryService.GetAllAsync(ct));
     }
     
     [HttpGet("{id}/products")]
-    public async Task<IActionResult> GetProducts(int id, int page = 1, int pageSize = 12,
-        decimal? minPrice = null, decimal? maxPrice = null)
+    public async Task<IActionResult> GetProducts(int id,
+        string? brand, bool? isAvailable , CancellationToken ct,
+        decimal? minPrice = null,  decimal? maxPrice = null, int page = 1,
+        int pageSize = 10)
     {
-        return Ok(await _countryService.GetProductByCountryIdAsync(id, page, pageSize, minPrice, maxPrice));
+        return Ok(await _countryService.GetProductByCountryIdAsync(id, brand, isAvailable,
+             ct, minPrice,  maxPrice, page, pageSize));
+    }
+    
+    
+    [Authorize(Roles = "Admin")]
+    [HttpPost("create")]
+    public async Task<IActionResult> Create( Country country, CancellationToken ct)
+    {
+        var id = await _countryService.CreateAsync(country.Name, country.ImageUrl, country.Code, ct);
+        return Ok( new
+        {
+            message = "A country was created",
+            id
+        });
+    }
+    
+    
+    [Authorize(Roles = "Admin")]
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Update(int id, string? name,
+        string? imageUrl, string? code, CancellationToken ct)
+    {
+        await _countryService.UpdateAsync(id,name, imageUrl,code, ct);
+        return Ok( new
+        {
+            message = "A country was updated",
+        });
+    }
+    
+    
+    [Authorize(Roles = "Admin")]
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> Delete(int id, CancellationToken ct)
+    {
+        await _countryService.DeleteAsync(id, ct);
+        return Ok( new
+        {
+            message = "A country was deleted",
+        });
     }
 }
