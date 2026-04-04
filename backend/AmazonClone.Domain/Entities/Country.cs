2120c478cab82eb2949c5cd686ee1eaf405b7a3d
krasutskaaa <krasutskadiana@gmail.com>
2026-04-04 22:05:40 +0300

    Some fields were set as nullable

--- backend/AmazonClone.Domain/Entities/Country.cs
+++ backend/AmazonClone.Domain/Entities/Country.cs
@@ -3,9 +3,9 @@ namespace AmazonClone.Domain.Entities;
 public class Country
 {
     public int Id { get; set; }
-    public string Name { get; set; }
-    public string ImageUrl { get; set; }
-    public string Code { get; set; }
-    public ICollection<Product> Products { get; set; } = new List<Product>();
+    public string? Name { get; set; }
+    public string? ImageUrl { get; set; }
+    public string? Code { get; set; }
+    public ICollection<Product>? Products { get; set; } = new List<Product>();
     //public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
 }
