21dcdd5dd38e1ceb59206728122fb52f1e8613d8
krasutskaaa <krasutskadiana@gmail.com>
2026-04-04 22:28:47 +0300

    Methods templates for admin create, update and delete were developed; the GetProductByCountryIdAsync method template was updated with new filter fields

--- backend/AmazonClone.Application/Interfaces/ICountryService.cs
+++ backend/AmazonClone.Application/Interfaces/ICountryService.cs
@@ -4,7 +4,11 @@ namespace AmazonClone.Application.Interfaces;
 
 public interface ICountryService
 {
-    Task<List<Product>> GetProductByCountryIdAsync(int countryId, int page = 1, int pageSize = 10,
-        decimal? minPrice = null, decimal? maxPrice = null);
-    Task<List<Country>> GetAllAsync();
+    Task<int> CreateAsync(string name, string imageUrl, string? code, CancellationToken ct);
+    Task UpdateAsync(int id, string? name, string? imageUrl, string? code, CancellationToken ct);
+    Task DeleteAsync(int id, CancellationToken ct);
+    Task<List<Product>> GetProductByCountryIdAsync(int countryId, string? brand,
+        bool? isAvailable, CancellationToken ct, decimal? minPrice = null, decimal? maxPrice = null, int page = 1,
+        int pageSize = 10);
+    Task<List<Country>> GetAllAsync(CancellationToken ct);
 }
