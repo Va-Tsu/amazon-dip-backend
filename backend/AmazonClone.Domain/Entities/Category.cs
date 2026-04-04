1d48eb4438672dd78b4b63df7e23342b84716b38
krasutskaaa <krasutskadiana@gmail.com>
2026-04-04 22:04:27 +0300

    Some fields were set as nullable

--- backend/AmazonClone.Domain/Entities/Category.cs
+++ backend/AmazonClone.Domain/Entities/Category.cs
@@ -3,7 +3,7 @@ namespace AmazonClone.Domain.Entities;
 public class Category
 {
     public int Id { get; set; }
-    public string Name { get; set; }
-    public string ImageUrl { get; set; }
-    public ICollection<Product>Products { get; set; }
+    public string? Name { get; set; }
+    public string? ImageUrl { get; set; }
+    public ICollection<Product>?Products { get; set; }
 }
