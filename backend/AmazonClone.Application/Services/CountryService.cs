d6ae0f510dcb8c923e6f827f1ce52466ca0a12d0
krasutskaaa <krasutskadiana@gmail.com>
2026-04-04 22:32:19 +0300

    New admin methods were inherited from the interface and were realized;the GetProductByCountryIdAsync method was updated

--- backend/AmazonClone.Application/Services/CountryService.cs
+++ backend/AmazonClone.Application/Services/CountryService.cs
@@ -14,10 +14,87 @@ public class CountryService:ICountryService
         _dbContext = dbContext;
     }
 
-    public async Task<List<Product>> GetProductByCountryIdAsync(int countryId, int page = 1, int pageSize = 10, decimal? minPrice = null,
-        decimal? maxPrice = null)
+    public async Task<int> CreateAsync(string name, string imageUrl, string? code, CancellationToken ct)
     {
-        var query = _dbContext.Products.Where(p=>p.CategoryId==countryId);
+        var exists = await _dbContext.Countries
+            .AnyAsync(c=>c.Name == name , ct);
+        if (exists)
+        {
+            throw new Exception($"Country with name {name} already exists");
+        }
+
+        var country = new Country()
+        {
+            Name = name,
+            ImageUrl = imageUrl,
+            Code = code
+        };
+        
+        _dbContext.Countries.Add(country);
+        await _dbContext.SaveChangesAsync(ct);
+
+        return country.Id;
+    }
+
+    public async Task UpdateAsync(int id, string? name, string? imageUrl, string? code, CancellationToken ct)
+    {
+        var country = await _dbContext.Countries
+            .FirstOrDefaultAsync(c => c.Id == id, ct);
+
+        if (country == null)
+        {
+            throw new Exception($"Country with id {id} not found");
+        }
+        
+        var exists = await _dbContext.Countries
+            .AnyAsync(c => c.Name == name && c.Id!=id, ct);
+        if (exists)
+        {
+            throw new Exception($"Country with name {name} already exists");
+        }
+        
+        country.Name = name;
+        country.ImageUrl = imageUrl ?? country.ImageUrl;
+        country.Code = code;
+        
+        await _dbContext.SaveChangesAsync(ct);
+    }
+
+    public async Task DeleteAsync(int id, CancellationToken ct)
+    {
+        var country = await _dbContext.Countries
+            .FirstOrDefaultAsync(c => c.Id == id, ct);
+
+        if (country == null)
+        {
+            throw new Exception($"Country with id {id} not found");
+        }
+        
+        _dbContext.Countries.Remove(country);
+        await _dbContext.SaveChangesAsync(ct);
+    }
+    
+
+    public async Task<List<Product>> GetProductByCountryIdAsync(int countryId,
+        string? brand, bool? isAvailable, CancellationToken ct, decimal? minPrice = null,
+        decimal? maxPrice = null,  int page = 1, int pageSize = 10)
+    {
+        var query = _dbContext.Products.AsQueryable();
+        query = _dbContext.Products.Where(p=>p.CountryId==countryId);
+
+        if (!string.IsNullOrWhiteSpace(brand))
+        {
+            query = query.Where(p => p.Brand == brand);
+        }
+        
+        if (isAvailable.HasValue)
+        {
+            if (isAvailable.Value)
+                query = query.Where(p => p.StockQuantity > 0);
+            else
+                query = query.Where(p => p.StockQuantity == 0);
+        }
+        
         if (minPrice.HasValue)
         {
             query = query.Where(p => p.Price >= minPrice.Value);
@@ -32,7 +109,7 @@ public class CountryService:ICountryService
         
     }
 
-    public async Task<List<Country>> GetAllAsync()
+    public async Task<List<Country>> GetAllAsync(CancellationToken ct)
     {
         return await _dbContext.Countries.ToListAsync();
     }
