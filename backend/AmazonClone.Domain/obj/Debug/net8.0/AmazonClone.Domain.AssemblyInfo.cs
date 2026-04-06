644ab30b3202c1a212232d5cee6f073778fb79b1
krasutskaaa <krasutskadiana@gmail.com>
2026-04-06 23:56:02 +0300

    Support files were added

--- backend/AmazonClone.Domain/obj/Debug/net8.0/AmazonClone.Domain.AssemblyInfo.cs
+++ backend/AmazonClone.Domain/obj/Debug/net8.0/AmazonClone.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AmazonClone.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1fe18a99804bcea518dbc08e11e2414558a5986")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a69dfa2f43c1778da19778a89a205bba4d0902ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("AmazonClone.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AmazonClone.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
