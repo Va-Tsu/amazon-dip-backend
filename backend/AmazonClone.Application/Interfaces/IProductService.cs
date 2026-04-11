6f5d5e992d6c1396c4ac487a496b415e33135b71
krasutskaaa <krasutskadiana@gmail.com>
2026-04-12 00:38:04 +0300

    The product service create, update and delete product method templates were changed

--- backend/AmazonClone.Application/Interfaces/IProductService.cs
+++ backend/AmazonClone.Application/Interfaces/IProductService.cs
@@ -1,4 +1,5 @@
 using AmazonClone.Domain.Entities;
+using AmazonClone.Domain.Enums;
 
 namespace AmazonClone.Application.Interfaces;
 
@@ -13,15 +14,17 @@ public interface IProductService
     Task<(List<Product>, bool hasMore)> GetRecommendedAsync(string? userId, int page,
         int pageSize, CancellationToken ct = default);
     Task AddRecentlyViewedAsync(string userId, Guid productId, CancellationToken ct = default);
-    Task<Guid> CreateAsync(Product product, CancellationToken ct = default);
+    Task<Guid> CreateAsync(Product product,List<string> imageUrls, CancellationToken ct = default);
 
     Task<bool> UpdateAsync(
-        Guid id, string? name, string? brand, string? description,
-        string? sku,  decimal? price, decimal? weight, decimal? parcelWeight,
-        int? categoryId, int? countryId, string? ingredients,
-        string? storageConditions, DateTime? expirationDate, string? article,
-        int? stockQuantity, bool? trackInventory, List<string>? imageUrls,
-        bool? isActive, CancellationToken ct = default);
+        Guid id, string? name, string? brand, string? description, string? sku, 
+        decimal? weight, decimal? parcelWeight, int? categoryId,
+        int? countryId, string? ingridients, string? storageConditions, 
+        DateTime? expirationDate, string? article, int? stockQuantity, 
+        int? lowStockTreshold, bool? trackInventory,
+        decimal? price, bool? hasDiscount, List<string>? imageUrls,
+        bool? isActive, bool? isPublished, Guid? sellerId, ProductStatus? status,
+        CancellationToken ct);
     
     Task<bool> DeleteAsync(Guid id, CancellationToken ct = default);
     
